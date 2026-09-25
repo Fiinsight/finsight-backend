bd44842ea9ea8d849e7affab65d6cc55fa43decd
subinpar <bagsubin565@gmail.com>
2026-09-25 22:16:48 +0900

    feat: map onboarding answers to learning preferences

--- src/main/java/com/finsight/auth/UserProfileDtos.java
+++ src/main/java/com/finsight/auth/UserProfileDtos.java
@@ -10,7 +10,8 @@ import java.util.List;
 public final class UserProfileDtos {
     private UserProfileDtos() {}
 
-    public record OnboardingAnswer(@NotBlank @Size(max = 200) String question,
+    public record OnboardingAnswer(@Size(max = 40) String questionId,
+                                   @NotBlank @Size(max = 200) String question,
                                    @NotBlank @Size(max = 200) String answer) {}
 
     public record OnboardingRequest(@NotEmpty @Size(max = 10) List<@Valid OnboardingAnswer> answers) {}
